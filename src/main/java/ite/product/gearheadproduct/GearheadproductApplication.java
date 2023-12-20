0e6fdd4e216e2eeaf06ffc1357c7c2e709abcc0b
Versna <phopversna007@gmail.com>
2023-12-20 14:20:31 +0700

    first

---
+++ src/main/java/ite/product/gearheadproduct/GearheadproductApplication.java
@@ -0,0 +1,13 @@
+package ite.product.gearheadproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GearheadproductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GearheadproductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ite/product/gearheadproduct/GearheadproductApplicationTests.java
@@ -0,0 +1,13 @@
+package ite.product.gearheadproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GearheadproductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
